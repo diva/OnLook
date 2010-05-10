8bcf573fe72964b4398a77a77970662a5685fcc9
Hazim Gazov <hazim.gazov@gmail.com>
2010-05-10 11:32:40 -0300

    allow anyone to view an object's properties

--- indra/newview/llpanelface.cpp
+++ indra/newview/llpanelface.cpp
@@ -383,9 +383,15 @@ void LLPanelFace::getState()
 
 	if( objectp
 		&& objectp->getPCode() == LL_PCODE_VOLUME
-		&& objectp->permModify())
+		// <edit>
+		//&& objectp->permModify())
+		)
+		// </edit>
 	{
-		BOOL editable = objectp->permModify();
+		// <edit>
+		//BOOL editable = objectp->permModify();
+		BOOL editable = TRUE;
+		// </edit>
 
 		// only turn on auto-adjust button if there is a media renderer and the media is loaded
 		childSetEnabled("textbox autofix",FALSE);
