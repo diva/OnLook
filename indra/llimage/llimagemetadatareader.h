d08468d0051e3286ec11159b35bcefd6fccfe549
Hazim Gazov <hazim.gazov@gmail.com>
2010-08-07 23:33:33 +0000

    add missing header file for last commit

--- indra/llimage/llimagemetadatareader.h
+++ indra/llimage/llimagemetadatareader.h
@@ -6,21 +6,27 @@
 #include <vector>
 #include <string>
 
+//encryption types
+#define ENC_NONE 0
+#define ENC_ONYXKDU 1
+#define ENC_EMKDU_V1 2
+#define ENC_EMKDU_V2 4
+
 class LLJ2cParser
 {
 public:
 	LLJ2cParser(U8* data,int data_size);
 	std::vector<U8> GetNextComment();
+	std::vector<U8> mData;
 private:
 	U8 nextChar();
 	std::vector<U8> nextCharArray(int len);
-	std::vector<U8> mData;
 	std::vector<U8>::iterator mIter;
 };
 class LLImageMetaDataReader
 {
 public:
-	static std::string ExtractEncodedComment(U8* data,int data_size);
+	static unsigned int ExtractEncodedComment(U8* data,int data_size, std::string& output);
 };
 #endif
 // </edit>
