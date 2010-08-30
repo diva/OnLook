bb89f6b9be30d9ad1a6d1fe04d5a447fc319f0fc
CharleyLevenque <charley.levenque@gmail.com>
2010-08-29 23:17:04 -0400

    Branding changes, Disable Login/Teleport screen support.

--- indra/newview/llfloaterabout.cpp
+++ indra/newview/llfloaterabout.cpp
@@ -123,10 +123,8 @@ LLFloaterAbout::LLFloaterAbout()
 		+ llformat(" %d.%d.%d (%d) %s %s (%s)\n",
 				   gSavedSettings.getU32("SpecifiedVersionMaj"), gSavedSettings.getU32("SpecifiedVersionMin"), gSavedSettings.getU32("SpecifiedVersionPatch"), gSavedSettings.getU32("SpecifiedVersionBuild"),
 				   __DATE__, __TIME__,
-    // <edit>
-	//			   gSavedSettings.getString("VersionChannelName").c_str());
 				   gSavedSettings.getString("SpecifiedChannel").c_str());
-	// </edit>
+
 	support_widget->appendColoredText(version, FALSE, FALSE, gColors.getColor("TextFgReadOnlyColor"));
 	support_widget->appendStyledText(LLTrans::getString("ReleaseNotes"), false, false, viewer_link_style);
 
