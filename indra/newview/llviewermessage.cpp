327d244e31c2222b4e0f0ac902784df431e90a57
CharleyLevenque <charley.levenque@gmail.com>
2010-09-01 03:42:39 -0400

    New settings keys, script change announcement, minimum zoom distance fixes.

--- indra/newview/llviewermessage.cpp
+++ indra/newview/llviewermessage.cpp
@@ -3805,6 +3805,25 @@ void process_sim_stats(LLMessageSystem *msg, void **user_data)
 			LLViewerStats::getInstance()->mSimChildAgents.addValue(stat_value);
 			break;
 		case LL_SIM_STAT_NUMSCRIPTSACTIVE:
+			if (gSavedSettings.getBOOL("AscentDisplayTotalScriptJumps"))
+			{
+				if(abs(stat_value-gSavedSettings.getF32("Ascentnumscripts"))>gSavedSettings.getF32("Ascentnumscriptdiff"))
+				{
+					LLChat chat;
+					std::stringstream os;
+					os << (U32)gSavedSettings.getF32("Ascentnumscripts");
+					std::stringstream ns;
+					ns << (U32)stat_value;
+					std::stringstream diff;
+					S32 tdiff = (stat_value-gSavedSettings.getF32("Ascentnumscripts"));
+					diff << tdiff;
+					std::string change_type = "";
+					if (tdiff > 0) change_type = "+";
+					chat.mText = "Total scripts jumped from " + os.str() + " to " + ns.str() + " ("+change_type+diff.str()+")";
+					LLFloaterChat::addChat(chat, FALSE,FALSE);
+				}
+				gSavedSettings.setF32("Ascentnumscripts",stat_value);
+			}
 			LLViewerStats::getInstance()->mSimActiveScripts.addValue(stat_value);
 			break;
 		case LL_SIM_STAT_SCRIPT_EPS:
