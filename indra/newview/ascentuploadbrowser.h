668b7f6096cd6955c3483f13c33fe6556d83fdc6
Beeks <HgDelirium@gmail.com>
2010-10-05 11:14:49 -0400

    Updates to Beeks' pet project.
    Signed-off-by: Beeks <HgDelirium@gmail.com>

--- indra/newview/ascentuploadbrowser.h
+++ indra/newview/ascentuploadbrowser.h
@@ -28,10 +28,11 @@ public:
 	static void onClickFile(LLUICtrl* ctrl, void* user_data);
 	static void onUpdateFilter(LLUICtrl* ctrl, void* user_data);
 	static void onDoubleClick(void* user_data);
-	
+	static void onDirCommit (LLUICtrl* ctrl, void* data);
 	static void onChangeDrives(LLUICtrl* ctrl, void* user_data);
 	static void onClickFilepathGoto(void* data);
 
+	void updateBrowser(void* data, std::string new_path);
 	void refresh();
 	void refreshUploadOptions();
 	void handleDoubleClick();
@@ -40,10 +41,12 @@ public:
 	std::vector<LLSD> datas;
 
 private:
+	static LLSD mUploaderSettings;
 	static ASFloaterUploadBrowser* sInstance;
 	enum FILE_COLUMN_ORDER
 	{
 		LIST_FILE_TYPE,
+		LIST_ASSET_TYPE,
 		LIST_INVENTORY_TYPE,
 		LIST_FILE_NAME,
 		LIST_DATA
@@ -53,6 +56,13 @@ private:
 		LIST_TYPE_PARENT,
 		LIST_TYPE_FOLDER,
 		LIST_TYPE_FILE
+	};
+	enum FILE_TYPE
+	{
+		FOLDER,
+		FILE_TEXTURE, 
+		FILE_SOUND,
+		FILE_ANIMATION
 	};
 	LLScrollListCtrl* mFileList;
 	LLComboBox* mDriveCombo;
