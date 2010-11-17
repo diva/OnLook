c1c2f65b12ae9962996015896283139b708fe1a4
Siana Gearz <siana.sg@live.de>
2010-11-17 02:39:27 +0100

    Integrated OpenJPEG 1.4

--- indra/cmake/OpenJPEG.cmake
+++ indra/cmake/OpenJPEG.cmake
@@ -1,22 +1,6 @@
 # -*- cmake -*-
-include(Prebuilt)
 
-set(OPENJPEG_FIND_QUIETLY ON)
-set(OPENJPEG_FIND_REQUIRED ON)
+# In-source library only need these variables set.
+set(OPENJPEG_LIBRARIES openjpeg)
+set(OPENJPEG_INCLUDE_DIR ${LIBS_OPEN_DIR}/libopenjpeg)
 
-if (STANDALONE)
-  include(FindOpenJPEG)
-else (STANDALONE)
-  use_prebuilt_binary(openjpeg)
-  
-  if(WINDOWS)
-    # Windows has differently named release and debug openjpeg(d) libs.
-    set(OPENJPEG_LIBRARIES 
-        debug openjpegd
-        optimized openjpeg)
-  else(WINDOWS)
-    set(OPENJPEG_LIBRARIES openjpeg)
-  endif(WINDOWS)
-  
-    set(OPENJPEG_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/include/openjpeg)
-endif (STANDALONE)
