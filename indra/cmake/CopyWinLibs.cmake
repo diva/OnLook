31b3c8e755135020becac34bf7c7788ad7343435
siana <siana.sg@live.de>
2011-02-21 15:15:55 +0100

    Singularity 1.5.4(2) fixes

--- indra/cmake/CopyWinLibs.cmake
+++ indra/cmake/CopyWinLibs.cmake
@@ -241,6 +241,8 @@ set(all_targets ${all_targets} ${out_targets})
 
 set(release_src_dir "${CMAKE_SOURCE_DIR}/../libraries/i686-win32/lib/release")
 set(release_files
+    libtcmalloc_minimal.dll
+    fmod.dll
     libapr-1.dll
     libaprutil-1.dll
     libapriconv-1.dll
