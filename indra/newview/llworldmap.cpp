1968cc602485f44ca746b790f73633f75deaf14e
Siana Gearz <siana.sg@live.de>
2011-02-25 20:30:31 +0100

    Ouch! sorry -.-

--- indra/newview/llworldmap.cpp
+++ indra/newview/llworldmap.cpp
@@ -735,9 +735,9 @@ void LLWorldMap::processMapBlockReply(LLMessageSystem* msg, void**)
 				callback(handle, LLWorldMap::getInstance()->mSLURL, image_id, LLWorldMap::getInstance()->mSLURLTeleport);
 			}
 		}
-		if(gAgent.mLureShow)
+		if(LLAgent::lure_show)
 		{
-			if((x_regions == gAgent.mLureGlobalX) && (y_regions == gAgent.mLureGlobalY))
+			if((x_regions == LLAgent::lure_global_x) && (y_regions == LLAgent::lure_global_y))
 			{
 				gAgent.onFoundLureDestination();
 			}
