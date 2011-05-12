ebe705352684758b6807d3ac493a7a7f07e97244
narv <narvczervik@gmail.com>
2011-05-12 07:45:13 -0700

    Version 1.5.7(2)

--- indra/llcommon/llversionviewer.h
+++ indra/llcommon/llversionviewer.h
@@ -36,12 +36,12 @@
 const S32 LL_VERSION_MAJOR = 1;
 const S32 LL_VERSION_MINOR = 5;
 const S32 LL_VERSION_PATCH = 7;
-const S32 LL_VERSION_BUILD = 1;
+const S32 LL_VERSION_BUILD = 2;
 
 const char * const LL_CHANNEL = "Singularity";
 
 #if LL_DARWIN
-const char * const LL_VERSION_BUNDLE_ID = "com.secondlife.snowglobe.viewer";
+const char * const LL_VERSION_BUNDLE_ID = "com.secondlife.singularity.viewer";
 #endif
 
 #endif
