3e84087d50d735e399b9c48e5b25e12294f676c6
Siana Gearz <siana.sg@live.de>
2011-07-13 04:39:42 +0200

    Modified fasttimer view, by Henri

--- indra/llui/llfloater.h
+++ indra/llui/llfloater.h
@@ -297,7 +297,9 @@ private:
 	bool			mDragOnLeft;
 
 	BOOL			mButtonsEnabled[BUTTON_COUNT];
+protected:
 	LLButton*		mButtons[BUTTON_COUNT];
+private:
 	F32				mButtonScale;
 	BOOL			mAutoFocus;
 	LLHandle<LLFloater> mSnappedTo;
