e06d0b2e70e35354bb90e893859a9256ba5ad31e
Siana Gearz <siana.sg@live.de>
2011-10-27 23:02:30 +0200

    Whoopsie!

--- indra/cmake/OpenGL.cmake
+++ indra/cmake/OpenGL.cmake
@@ -7,4 +7,4 @@ if (NOT STANDALONE OR DARWIN)
   #use_prebuilt_binary(glh_linear)
   # actually... not any longer, it's now in git -SG
   set(GLEXT_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/include)
-endif (NOT STANDALONE)
+endif ()
