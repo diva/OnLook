60081d7ed5d699eb97027c48e7431481473bcde2
Siana Gearz <siana.sg@live.de>
2012-02-16 13:22:55 +0100

    Melanie's fix of FMOD on OS X

--- indra/cmake/FMOD.cmake
+++ indra/cmake/FMOD.cmake
@@ -7,25 +7,12 @@ if(INSTALL_PROPRIETARY)
   use_prebuilt_binary(fmod)
 endif(INSTALL_PROPRIETARY)
 
-find_library(FMOD_LIBRARY_RELEASE
+find_library(FMOD_LIBRARY
              NAMES fmod fmodvc fmod-3.75
              PATHS
-             ${ARCH_PREBUILT_DIRS_RELEASE}
+             optimized ${ARCH_PREBUILT_DIRS_RELEASE}
+             debug ${ARCH_PREBUILT_DIRS_DEBUG}
              )
-
-find_library(FMOD_LIBRARY_DEBUG
-             NAMES fmod fmodvc fmod-3.75
-             PATHS
-             ${ARCH_PREBUILT_DIRS_DEBUG}
-             )
-
-if (FMOD_LIBRARY_RELEASE AND FMOD_LIBRARY_DEBUG)
-  set(FMOD_LIBRARY
-      debug ${FMOD_LIBRARY_DEBUG}
-      optimized ${FMOD_LIBRARY_RELEASE})
-elseif (FMOD_LIBRARY_RELEASE)
-  set(FMOD_LIBRARY ${FMOD_LIBRARY_RELEASE})
-endif (FMOD_LIBRARY_RELEASE AND FMOD_LIBRARY_DEBUG)
 
 if (NOT FMOD_LIBRARY)
   set(FMOD_SDK_DIR CACHE PATH "Path to the FMOD SDK.")
