35a53ef1d76c193c3a0031d5d8eb5fa2037b69d0
Aleric Inglewood <Aleric.Inglewood@gmail.com>
2012-08-13 21:59:57 +0200

    Bug fix (needed for libcwd with memory allocation debugging)

--- indra/llmessage/llhttpclient.cpp
+++ indra/llmessage/llhttpclient.cpp
@@ -123,7 +123,7 @@ namespace
 	{
 	public:
 		RawInjector(const U8* data, S32 size) : mData(data), mSize(size) {}
-		virtual ~RawInjector() {delete mData;}
+		virtual ~RawInjector() {delete [] mData;}
 
 		const char* contentType() { return "application/octet-stream"; }
 
