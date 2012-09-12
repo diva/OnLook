diva/OnLook

commit f77f846c8dfa185aa9c7a07fe51840e21679a9c2

Anim upload issues fixed, bulk upload clarified.

indra/newview/llfloaternamedesc.cpp
@@ -157,7 +157,7 @@ BOOL LLFloaterNameDesc::postBuild()
 
 	// OK button
 	childSetLabelArg("ok_btn", "[UPLOADFEE]", gHippoGridManager->getConnectedGrid()->getUploadFee());
-	childSetAction("ok_btn", onBtnOK, this);
+	//childSetAction("ok_btn", onBtnOK, this);
 	setDefaultBtn("ok_btn");
 
 	return TRUE;

indra/newview/CMakeLists.txt
@@ -83,7 +83,6 @@ set(viewer_SOURCE_FILES
     emeraldboobutils.cpp
     floaterao.cpp
     floaterlocalassetbrowse.cpp
-    floatersculptpreview.cpp
     floatervoicelicense.cpp
     hbfloatergrouptitles.cpp
     hgfloatertexteditor.cpp
@@ -584,7 +583,6 @@ set(viewer_HEADER_FILES
     emeraldboobutils.h
     floaterao.h
     floaterlocalassetbrowse.h
-    floatersculptpreview.h
     floatervoicelicense.h
     hbfloatergrouptitles.h
     hgfloatertexteditor.h

indra/newview/floatersculptpreview.h
@@ -1,144 +1,0 @@
-/** 
- * @file LLFloaterSculptPreview.h
- * @brief LLFloaterSculptPreview class definition
- *
- * $LicenseInfo:firstyear=2004&license=viewergpl$
- * 
- * Copyright (c) 2004-2009, Linden Research, Inc.
- * 
- * Second Life Viewer Source Code
- * The source code in this file ("Source Code") is provided by Linden Lab
- * to you under the terms of the GNU General Public License, version 2.0
- * ("GPL"), unless you have obtained a separate licensing agreement
- * ("Other License"), formally executed by you and Linden Lab.  Terms of
- * the GPL can be found in doc/GPL-license.txt in this distribution, or
- * online at http://secondlifegrid.net/programs/open_source/licensing/gplv2
- * 
- * There are special exceptions to the terms and conditions of the GPL as
- * it is applied to this Source Code. View the full text of the exception
- * in the file doc/FLOSS-exception.txt in this software distribution, or
- * online at
- * http://secondlifegrid.net/programs/open_source/licensing/flossexception
- * 
- * By copying, modifying or distributing this software, you acknowledge
- * that you have read and understood your obligations described above,
- * and agree to abide by those obligations.
- * 
- * ALL LINDEN LAB SOURCE CODE IS PROVIDED "AS IS." LINDEN LAB MAKES NO
- * WARRANTIES, EXPRESS, IMPLIED OR OTHERWISE, REGARDING ITS ACCURACY,
- * COMPLETENESS OR PERFORMANCE.
- * $/LicenseInfo$
- */
-
-#ifndef LL_LLFloaterSculptPreview_H
-#define LL_LLFloaterSculptPreview_H
-
-#include "llfloater.h"
-#include "llresizehandle.h"
-#include "lldynamictexture.h"
-#include "llquaternion.h"
-#include "llviewerobjectlist.h"
-
-class LLComboBox;
-class LLJoint;
-class LLViewerJointMesh;
-class LLVOAvatar;
-class LLTextBox;
-class LLVertexBuffer;
-
-class LLPreviewSculpted : public LLViewerDynamicTexture
-{
- public:
-	LLPreviewSculpted(S32 width, S32 height);
-	virtual ~LLPreviewSculpted();
-
-	void setPreviewTarget(LLImageRaw *imagep, F32 distance);
-	void setTexture(U32 name) { mTextureName = name; }
-
-	BOOL render();
-	void refresh();
-	void rotate(F32 yaw_radians, F32 pitch_radians);
-	void zoom(F32 zoom_amt);
-	void pan(F32 right, F32 up);
-	virtual BOOL needsRender() { return mNeedsUpdate; }
-
- protected:
-	BOOL        mNeedsUpdate;
-	U32         mTextureName;
-	F32			mCameraDistance;
-	F32			mCameraYaw;
-	F32			mCameraPitch;
-	F32			mCameraZoom;
-	LLVector3	mCameraOffset;
-	LLPointer<LLVolume> mVolume;
-	LLPointer<LLVertexBuffer> mVertexBuffer;
-};
-
-
-class LLPreviewAvatar : public LLViewerDynamicTexture
-{
-public:
-	LLPreviewAvatar(S32 width, S32 height);
-	virtual ~LLPreviewAvatar();
-
-	void setPreviewTarget(const std::string& joint_name, const std::string& mesh_name, LLImageRaw* imagep, F32 distance, BOOL male);
-	void setTexture(U32 name) { mTextureName = name; }
-	void clearPreviewTexture(const std::string& mesh_name);
-
-	BOOL	render();
-	void	refresh();
-	void	rotate(F32 yaw_radians, F32 pitch_radians);
-	void	zoom(F32 zoom_amt);
-	void	pan(F32 right, F32 up);
-	virtual BOOL needsRender() { return mNeedsUpdate; }
-
-protected:
-	BOOL		mNeedsUpdate;
-	LLJoint*	mTargetJoint;
-	LLViewerJointMesh*	mTargetMesh;
-	F32			mCameraDistance;
-	F32			mCameraYaw;
-	F32			mCameraPitch;
-	F32			mCameraZoom;
-	LLVector3	mCameraOffset;
-	LLPointer<LLVOAvatar> mDummyAvatar;
-	U32			mTextureName;
-};
-
-class LLFloaterSculptPreview : public LLFloater
-{
-public:
-	LLFloaterSculptPreview(LLImageRaw* src);
-	virtual ~LLFloaterSculptPreview();
-	static LLFloaterSculptPreview* show(LLImageRaw* src);
-	virtual BOOL postBuild();
-	
-	BOOL handleMouseDown(S32 x, S32 y, MASK mask);
-	BOOL handleMouseUp(S32 x, S32 y, MASK mask);
-	BOOL handleHover(S32 x, S32 y, MASK mask);
-	BOOL handleScrollWheel(S32 x, S32 y, S32 clicks); 
-
-	static void onMouseCaptureLostImagePreview(LLMouseHandler*);
-	static void setUploadAmount(S32 amount) { sUploadAmount = amount; }
-
-	void clearAllPreviewTextures();
-
-protected:
-	static void		onPreviewTypeCommit(LLUICtrl*,void*);
-	void			draw();
-	bool			loadImage(LLImageRaw* src);
-
-	LLPointer<LLImageRaw> mRawImagep;
-	LLPointer<LLPreviewAvatar> mAvatarPreview;
-	LLPointer<LLPreviewSculpted> mSculptedPreview;
-	S32				mLastMouseX;
-	S32				mLastMouseY;
-	LLRect			mPreviewRect;
-	LLRectf			mPreviewImageRect;
-	LLPointer<LLViewerTexture> mImagep ;
-	LLViewerObject* tmpvolume;
-	
-	static S32		sUploadAmount;
-};
-
-#endif  // LL_LLFloaterSculptPreview_H

indra/newview/llfloateranimpreview.cpp
@@ -1,4 +1,4 @@
-/** 
+/**
  * @file llfloateranimpreview.cpp
  * @brief LLFloaterAnimPreview class implementation
  *
@@ -1430,17 +1430,18 @@ void LLFloaterAnimPreview::onBtnOK(void* userdata)
 				}
 				else
 				// </edit>
-
-				upload_new_resource(floaterp->mTransactionID, // tid
+				{
+					upload_new_resource(floaterp->mTransactionID, // tid
 						    LLAssetType::AT_ANIMATION,
 						    name,
 						    desc,
 						    0,
-						    LLFolderType::FT_ANIMATION,
+						    LLFolderType::FT_NONE,
 						    LLInventoryType::IT_ANIMATION,
 						    LLFloaterPerms::getNextOwnerPerms(), LLFloaterPerms::getGroupPerms(), LLFloaterPerms::getEveryonePerms(),
 						    name,
 						    callback, expected_upload_cost, userdata);
+				}
 			}
 			else
 			{

indra/newview/llfloaterimagepreview.cpp
@@ -111,6 +111,7 @@ BOOL LLFloaterImagePreview::postBuild()
 	}
 
 	childSetLabelArg("ok_btn", "[UPLOADFEE]", gHippoGridManager->getConnectedGrid()->getUploadFee());
+	childSetAction("ok_btn", onBtnOK, this);
 
 	LLCtrlSelectionInterface* iface = childGetSelectionInterface("clothing_type_combo");
 	if (iface)

indra/newview/llfloaterurlentry.h
@@ -1,6 +1,6 @@
 /**
- * @file llfloaternamedesc.h
- * @brief LLFloaterNameDesc class definition
+ * @file llfloaterurlentry.h
+ * @brief LLFloaterURLEntry class definition
  *
  * $LicenseInfo:firstyear=2007&license=viewergpl$
  * 

indra/newview/llviewermenufile.cpp
@@ -359,12 +359,10 @@ class LLFileUploadBulk : public view_listener_t
 		// Also fix single upload to charge first, then refund
 		// <edit>
 		S32 expected_upload_cost = LLGlobalEconomy::Singleton::getInstance()->getPriceUpload();
+		const char* notification_type = expected_upload_cost ? "BulkTemporaryUpload" : "BulkTemporaryUploadFree";
 		LLSD args;
-		std::string msg = "Would you like to bulk upload the files as temporary files?\nOnly textures will upload as temporary on Agni and Aditi.";
-		if(expected_upload_cost)
-			msg.append(llformat("\nWARNING: Each upload costs L$%d if it's not temporary.",expected_upload_cost));
-		args["MESSAGE"] = msg;
-		LLNotifications::instance().add("GenericAlertYesNoCancel", args, LLSD(), onConfirmBulkUploadTemp);
+		args["UPLOADCOST"] = gHippoGridManager->getConnectedGrid()->getUploadFee();
+		LLNotifications::instance().add(notification_type, args, LLSD(), onConfirmBulkUploadTemp);
 		return true;
 	}
 

commit b663110288fbd25c17ceaaab9a9fa0d3c568a6ca

Fix Sound Upload not working
Was broken by a change in f77f846c8dfa185aa9c7a07fe51840e21679a9c2
Thanks to Lucia TopHat and Tazy Scientist for helping me test and confirm all this.

indra/newview/llfloaternamedesc.cpp
@@ -157,7 +157,10 @@ BOOL LLFloaterNameDesc::postBuild()
 
 	// OK button
 	childSetLabelArg("ok_btn", "[UPLOADFEE]", gHippoGridManager->getConnectedGrid()->getUploadFee());
-	//childSetAction("ok_btn", onBtnOK, this);
+	if (exten == "wav")
+	{
+		childSetAction("ok_btn", onBtnOK, this);
+	}
 	setDefaultBtn("ok_btn");
 
 	return TRUE;