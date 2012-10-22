diva/OnLook

commit 0bee4a922f2bc84796d901f83766ecfe162f0f2a

Make sure that sRunning <= number of threads with status RUNNING.

indra/llcommon/llthread.cpp
@@ -104,6 +104,11 @@ void *APR_THREAD_FUNC LLThread::staticRun(apr_thread_t *apr_threadp, void *datap
 	// the moment it happens... therefore make a copy here.
 	char const* volatile name = threadp->mName.c_str();
 	
+	// Always make sure that sRunning <= number of threads with status RUNNING,
+	// so do this before changing mStatus (meaning that once we see that we
+	// are STOPPED, then sRunning is also up to date).
+	--sRunning;
+
 	// We're done with the run function, this thread is done executing now.
 	threadp->mStatus = STOPPED;
 

commit daac25c3434717842f7e7be6389867583e34c0a6

Bug fix, introduced in commit 0bee4a92

indra/llcommon/llthread.cpp
@@ -118,7 +118,6 @@ void *APR_THREAD_FUNC LLThread::staticRun(apr_thread_t *apr_threadp, void *datap
 	// the critical area of the mSignal lock)].
 	lldebugs << "LLThread::staticRun() Exiting: " << name << llendl;
 
-	--sRunning;		// Would be better to do this after joining with the thread, but we don't join :/
 	return NULL;
 }
 