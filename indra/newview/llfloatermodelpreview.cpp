909247a1798a6c88ec2a8efe627000ed3685fa5d
Aleric Inglewood <Aleric.Inglewood@gmail.com>
2013-01-13 19:46:35 +0100

    Keep decimal format the default.

--- indra/newview/llfloatermodelpreview.cpp
+++ indra/newview/llfloatermodelpreview.cpp
@@ -338,7 +338,7 @@ BOOL stop_gloderror()
 
 	if (error != GLOD_NO_ERROR)
 	{
-		llwarns << "GLOD error detected, cannot generate LOD: " << std::hex << error << llendl;
+		llwarns << "GLOD error detected, cannot generate LOD: " << std::hex << error << std::dec << llendl;
 		return TRUE;
 	}
 
