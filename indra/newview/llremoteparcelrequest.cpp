085d6cc80647655dfa6c92019826fe60680edaff
Lirusaito <inusaito@gmail.com>
2013-02-09 17:06:38 -0500

    Derive LLPanelPlace from LLRemoteParcelInfoObserver, and update affected sections to v-d.

--- indra/newview/llremoteparcelrequest.cpp
+++ indra/newview/llremoteparcelrequest.cpp
@@ -34,46 +34,172 @@
 
 #include "llviewerprecompiledheaders.h"
 
-#include "llagent.h"
-#include "llremoteparcelrequest.h"
+#include "message.h"
 
-#include "llpanelplace.h"
 #include "llpanel.h"
 #include "llhttpclient.h"
 #include "llsdserialize.h"
+//#include "llurlentry.h"
 #include "llviewerregion.h"
 #include "llview.h"
-#include "message.h"
 
-LLRemoteParcelRequestResponder::LLRemoteParcelRequestResponder(LLHandle<LLPanel> place_panel_handle)
-{
-	 mPlacePanelHandle = place_panel_handle;
-}
-/*virtual*/
+#include "llagent.h"
+#include "llremoteparcelrequest.h"
+
+
+LLRemoteParcelRequestResponder::LLRemoteParcelRequestResponder(LLHandle<LLRemoteParcelInfoObserver> observer_handle)
+	 : mObserverHandle(observer_handle)
+{}
+
+//If we get back a normal response, handle it here
+//virtual
 void LLRemoteParcelRequestResponder::result(const LLSD& content)
 {
 	LLUUID parcel_id = content["parcel_id"];
 
-	LLPanelPlace* place_panelp = (LLPanelPlace*)mPlacePanelHandle.get();
-
-	if(place_panelp)
+	// Panel inspecting the information may be closed and destroyed
+	// before this response is received.
+	LLRemoteParcelInfoObserver* observer = mObserverHandle.get();
+	if (observer)
 	{
-		place_panelp->setParcelID(parcel_id);
+		observer->setParcelID(parcel_id);
 	}
-
 }
 
-/*virtual*/
+//If we get back an error (not found, etc...), handle it here
+//virtual
 void LLRemoteParcelRequestResponder::error(U32 status, const std::string& reason)
 {
 	llinfos << "LLRemoteParcelRequest::error("
 		<< status << ": " << reason << ")" << llendl;
-	LLPanelPlace* place_panelp = (LLPanelPlace*)mPlacePanelHandle.get();
 
-	if(place_panelp)
+	// Panel inspecting the information may be closed and destroyed
+	// before this response is received.
+	LLRemoteParcelInfoObserver* observer = mObserverHandle.get();
+	if (observer)
 	{
-		place_panelp->setErrorStatus(status, reason);
+		observer->setErrorStatus(status, reason);
 	}
-
 }
 
+void LLRemoteParcelInfoProcessor::addObserver(const LLUUID& parcel_id, LLRemoteParcelInfoObserver* observer)
+{
+	observer_multimap_t::iterator it;
+	observer_multimap_t::iterator start = mObservers.lower_bound(parcel_id);
+	observer_multimap_t::iterator end = mObservers.upper_bound(parcel_id);
+
+	// Check if the observer is already in observers list for this UUID
+	for(it = start; it != end; ++it)
+	{
+		if (it->second.get() == observer)
+		{
+			return;
+		}
+	}
+
+	mObservers.insert(std::make_pair(parcel_id, observer->getObserverHandle()));
+}
+
+void LLRemoteParcelInfoProcessor::removeObserver(const LLUUID& parcel_id, LLRemoteParcelInfoObserver* observer)
+{
+	if (!observer)
+	{
+		return;
+	}
+
+	observer_multimap_t::iterator it;
+	observer_multimap_t::iterator start = mObservers.lower_bound(parcel_id);
+	observer_multimap_t::iterator end = mObservers.upper_bound(parcel_id);
+
+	for(it = start; it != end; ++it)
+	{
+		if (it->second.get() == observer)
+		{
+			mObservers.erase(it);
+			break;
+		}
+	}
+}
+
+//static
+void LLRemoteParcelInfoProcessor::processParcelInfoReply(LLMessageSystem* msg, void**)
+{
+	LLParcelData parcel_data;
+
+	msg->getUUID	("Data", "ParcelID", parcel_data.parcel_id);
+	msg->getUUID	("Data", "OwnerID", parcel_data.owner_id);
+	msg->getString	("Data", "Name", parcel_data.name);
+	msg->getString	("Data", "Desc", parcel_data.desc);
+	msg->getS32		("Data", "ActualArea", parcel_data.actual_area);
+	msg->getS32		("Data", "BillableArea", parcel_data.billable_area);
+	msg->getU8		("Data", "Flags", parcel_data.flags);
+	msg->getF32		("Data", "GlobalX", parcel_data.global_x);
+	msg->getF32		("Data", "GlobalY", parcel_data.global_y);
+	msg->getF32		("Data", "GlobalZ", parcel_data.global_z);
+	msg->getString	("Data", "SimName", parcel_data.sim_name);
+	msg->getUUID	("Data", "SnapshotID", parcel_data.snapshot_id);
+	msg->getF32		("Data", "Dwell", parcel_data.dwell);
+	msg->getS32		("Data", "SalePrice", parcel_data.sale_price);
+	msg->getS32		("Data", "AuctionID", parcel_data.auction_id);
+
+	LLRemoteParcelInfoProcessor::observer_multimap_t & observers = LLRemoteParcelInfoProcessor::getInstance()->mObservers;
+
+	typedef std::vector<observer_multimap_t::iterator> deadlist_t;
+	deadlist_t dead_iters;
+
+	observer_multimap_t::iterator oi = observers.lower_bound(parcel_data.parcel_id);
+	observer_multimap_t::iterator end = observers.upper_bound(parcel_data.parcel_id);
+
+	while (oi != end)
+	{
+		// increment the loop iterator now since it may become invalid below
+		observer_multimap_t::iterator cur_oi = oi++;
+
+		LLRemoteParcelInfoObserver * observer = cur_oi->second.get();
+		if(observer)
+		{
+			// may invalidate cur_oi if the observer removes itself
+			observer->processParcelInfo(parcel_data);
+		}
+		else
+		{
+			// the handle points to an expired observer, so don't keep it
+			// around anymore
+			dead_iters.push_back(cur_oi);
+		}
+	}
+
+	deadlist_t::iterator i;
+	deadlist_t::iterator end_dead = dead_iters.end();
+	for(i = dead_iters.begin(); i != end_dead; ++i)
+	{
+		observers.erase(*i);
+	}
+
+#ifdef LL_LLURLENTRY_H
+	LLUrlEntryParcel::LLParcelData url_data;
+	url_data.parcel_id = parcel_data.parcel_id;
+	url_data.name = parcel_data.name;
+	url_data.sim_name = parcel_data.sim_name;
+	url_data.global_x = parcel_data.global_x;
+	url_data.global_y = parcel_data.global_y;
+	url_data.global_z = parcel_data.global_z;
+
+	// Pass the parcel data to LLUrlEntryParcel to render
+	// human readable parcel name.
+	LLUrlEntryParcel::processParcelInfo(url_data);
+#endif //LL_LLURLENTRY_H
+}
+
+void LLRemoteParcelInfoProcessor::sendParcelInfoRequest(const LLUUID& parcel_id)
+{
+	LLMessageSystem *msg = gMessageSystem;
+
+	msg->newMessage("ParcelInfoRequest");
+	msg->nextBlockFast(_PREHASH_AgentData);
+	msg->addUUIDFast(_PREHASH_AgentID, gAgent.getID() );
+	msg->addUUID("SessionID", gAgent.getSessionID());
+	msg->nextBlock("Data");
+	msg->addUUID("ParcelID", parcel_id);
+	gAgent.sendReliableMessage();
+}
