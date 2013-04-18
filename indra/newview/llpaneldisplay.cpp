0261f13670fe1a47a1bba6e32252354bdb6cdbb8
Shyotl <Shyotl@gmail.com>
2013-04-18 15:54:05 -0500

    LLSliderCtrl callbacks actually get the child LLSlider object, not the LLSliderCtrl object.

--- indra/newview/llpaneldisplay.cpp
+++ indra/newview/llpaneldisplay.cpp
@@ -885,7 +885,7 @@ void LLPanelDisplay::apply()
 
 void LLPanelDisplay::onChangeQuality(LLUICtrl* caller)
 {
-	LLSliderCtrl* sldr = static_cast<LLSliderCtrl*>(caller);
+	LLSlider* sldr = dynamic_cast<LLSlider*>(caller);
 
 	if(sldr == NULL)
 	{
