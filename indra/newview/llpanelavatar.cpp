7e874d7d56084f42061e5d216dfe685826b70932
Lirusaito <inusaito@gmail.com>
2013-07-10 07:35:50 -0400

    Fix Issue 931: When viewing the profile of a muted avatar, the Mute button should be changed to Unmute

--- indra/newview/llpanelavatar.cpp
+++ indra/newview/llpanelavatar.cpp
@@ -520,7 +520,7 @@ BOOL LLPanelAvatarSecondLife::postBuild(void)
 
 	getChild<LLUICtrl>("Add Friend...")->setCommitCallback(boost::bind(LLAvatarActions::requestFriendshipDialog, boost::bind(&LLPanelAvatar::getAvatarID, pa)));
 	getChild<LLUICtrl>("Pay...")->setCommitCallback(boost::bind(LLAvatarActions::pay, boost::bind(&LLPanelAvatar::getAvatarID, pa)));
-	childSetAction("Mute", LLPanelAvatar::onClickMute, pa);
+	getChild<LLUICtrl>("Mute")->setCommitCallback(boost::bind(LLAvatarActions::toggleBlock, boost::bind(&LLPanelAvatar::getAvatarID, pa)));
 
 	getChild<LLUICtrl>("Offer Teleport...")->setCommitCallback(boost::bind(static_cast<void(*)(const LLUUID&)>(LLAvatarActions::offerTeleport), boost::bind(&LLPanelAvatar::getAvatarID, pa)));
 
@@ -1725,26 +1725,6 @@ void LLPanelAvatar::onClickGetKey(void *userdata)
 	llinfos << "Copy agent id: " << agent_id << llendl;
 
 	gViewerWindow->mWindow->copyTextToClipboard(utf8str_to_wstring(agent_id.asString()));
-}
-
-//-----------------------------------------------------------------------------
-// onClickMute()
-//-----------------------------------------------------------------------------
-void LLPanelAvatar::onClickMute(void *userdata)
-{
-	LLPanelAvatar* self = (LLPanelAvatar*) userdata;
-	
-	LLUUID agent_id = self->getAvatarID();
-
-	LLFloaterMute::showInstance();
-	if (LLAvatarActions::isBlocked(agent_id))
-	{
-		LLFloaterMute::getInstance()->selectMute(agent_id);
-	}
-	else
-	{
-		LLAvatarActions::toggleBlock(agent_id);
-	}
 }
 
 // static
