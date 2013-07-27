641067c3991d1bfa17a5d6ae21ce71010ea8319c
Inusaito Sayori <Inusaito@gmail.com>
2013-07-27 15:47:04 -0400

    [Voice Update] Apply updates from Viewer-Vivox2
    Applies the following commits:
     4c13e7c - Interim version with SLIM removed from voice.
       https://bitbucket.org/obscurestar/viewer-vivox2/commits/4c13e7c7535cbae2767181844a014897c85f254a
     9cad21a - Thanks mercurial.
       https://bitbucket.org/obscurestar/viewer-vivox2/commits/9cad21adc15947f74a1beda31264730361fc6569
       Only the changes that should have been in 4c13e7c (removal of LLVivoxVoiceClient::accountListBlockRulesSendMessage() and LLVivoxVoiceClient::accountListAutoAcceptRulesSendMessage())
     9c94b96 - Removed debugging.
       https://bitbucket.org/obscurestar/viewer-vivox2/commits/9c94b96ed3ac20987f308b5296fba1cc51a875b1
     ec77595 - Turn off logging.
       https://bitbucket.org/obscurestar/viewer-vivox2/commits/ec775958ef8c2923ddc03ed051e322310dc278cd
     857cab1 - Kill all the vivox logs
       https://bitbucket.org/obscurestar/viewer-vivox2/commits/857cab153e835211d0a76e66d627a14de0f4706b

--- indra/newview/llinventorybridge.cpp
+++ indra/newview/llinventorybridge.cpp
@@ -3327,7 +3327,7 @@ void LLFolderBridge::buildContextMenuBaseOptions(U32 flags)
 			}
 		}
 
-		//Added by spatters to force inventory pull on right-click to display folder options correctly. 07-17-06
+		//Added by aura to force inventory pull on right-click to display folder options correctly. 07-17-06
 		mCallingCards = mWearables = FALSE;
 
 		LLIsType is_callingcard(LLAssetType::AT_CALLINGCARD);
