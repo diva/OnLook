3d6b94d2edaffda15cd116178a0b13b1e1b1d713
Latif Khalifa <latifer@streamgrid.net>
2013-09-11 23:28:26 +0200

    Avoid infinite recursion crashes with wider octree range

--- indra/llmath/lloctree.h
+++ indra/llmath/lloctree.h
@@ -623,6 +623,9 @@ public:
 				child->insert(data);
 			}
 		}
+// Singu note: now that we allow wider range in octree, discard them here
+// if they fall out of range
+#if 0
 		else 
 		{
 			//it's not in here, give it to the root
@@ -641,6 +644,7 @@ public:
 				node->insert(data);
 			}
 		}
+#endif
 
 		return false;
 	}
