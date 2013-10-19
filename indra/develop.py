d03a012a41507bb6fe63f69850f96051bb74338d
Latif Khalifa <latifer@streamgrid.net>
2013-10-19 21:10:24 +0200

    Added support for Visual Studio 2012 to develop.py and VSTool

--- indra/develop.py
+++ indra/develop.py
@@ -443,9 +443,14 @@ class WindowsSetup(PlatformSetup):
         'vc100' : {
             'gen' : r'Visual Studio 10',
             'ver' : r'10.0'
+            },
+        'vc110' : {
+            'gen' : r'Visual Studio 11',
+            'ver' : r'11.0'
             }
         }
     gens['vs2010'] = gens['vc100']
+    gens['vs2012'] = gens['vc110']
 
     search_path = r'C:\windows'
     exe_suffixes = ('.exe', '.bat', '.com')
