c6ec03ab19b55e92018cfd15f3ff554205c59758
Latif Khalifa <latifer@streamgrid.net>
2013-11-19 00:52:59 +0100

    Re-enable generating tarballs when packaging on Linux

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -806,12 +806,11 @@ class LinuxManifest(ViewerManifest):
         try:
             # --numeric-owner hides the username of the builder for
             # security etc.
-            # I'm leaving this disabled for speed
-            #self.run_command("tar -C '%(dir)s' --numeric-owner -cjf "
-            #                 "'%(inst_path)s.tar.bz2' %(inst_name)s" % {
-            #    'dir': self.get_build_prefix(),
-            #    'inst_name': installer_name,
-            #    'inst_path':self.build_path_of(installer_name)})
+            self.run_command("tar -C '%(dir)s' --numeric-owner -cjf "
+                             "'%(inst_path)s.tar.bz2' %(inst_name)s" % {
+                'dir': self.get_build_prefix(),
+                'inst_name': installer_name,
+                'inst_path':self.build_path_of(installer_name)})
             print ''
         finally:
             self.run_command("mv '%(inst)s' '%(dst)s'" % {
