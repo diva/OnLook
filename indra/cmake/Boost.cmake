85c875e85698977e6599800c6631a40955dcb8c6
Latif Khalifa <latifer@streamgrid.net>
2013-12-20 03:02:17 +0100

    Link boost dynamically, bundle its libs. Addresses issue 1152.
    Rebuild collada to also use dynamic boost.
    SV-1152 #close Fixed by linking boost dynamically

--- indra/cmake/Boost.cmake
+++ indra/cmake/Boost.cmake
@@ -38,26 +38,26 @@ else (STANDALONE)
         debug libboost_thread-mt-gd)
   elseif (LINUX)
     set(Boost_CONTEXT_LIBRARY
-        optimized boost_context-mt.a
-        debug boost_context-mt-d.a)
+        optimized boost_context-mt
+        debug boost_context-mt-d)
     set(Boost_FILESYSTEM_LIBRARY
-        optimized boost_filesystem-mt.a
-        debug boost_filesystem-mt-d.a)
+        optimized boost_filesystem-mt
+        debug boost_filesystem-mt-d)
     set(Boost_PROGRAM_OPTIONS_LIBRARY
-        optimized boost_program_options-mt.a
-        debug boost_program_options-mt-d.a)
+        optimized boost_program_options-mt
+        debug boost_program_options-mt-d)
     set(Boost_REGEX_LIBRARY
-        optimized boost_regex-mt.a
-        debug boost_regex-mt-d.a)
+        optimized boost_regex-mt
+        debug boost_regex-mt-d)
     set(Boost_SIGNALS_LIBRARY
-        optimized boost_signals-mt.a
-        debug boost_signals-mt-d.a)
+        optimized boost_signals-mt
+        debug boost_signals-mt-d)
     set(Boost_SYSTEM_LIBRARY
-        optimized boost_system-mt.a
-        debug boost_system-mt-d.a)
+        optimized boost_system-mt
+        debug boost_system-mt-d)
     set(Boost_THREAD_LIBRARY
-        optimized boost_thread-mt.a
-        debug boost_thread-mt-d.a)
+        optimized boost_thread-mt
+        debug boost_thread-mt-d)
   elseif (DARWIN)
     set(Boost_CONTEXT_LIBRARY
         optimized boost_context-mt
