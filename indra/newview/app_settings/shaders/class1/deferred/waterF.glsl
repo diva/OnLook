e2fa94e2c3b9afd493337dd494fa38aff629a284
Shyotl <Shyotl@gmail.com>
2014-07-05 19:09:21 -0500

    Fixed water normals.

--- indra/newview/app_settings/shaders/class1/deferred/waterF.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/waterF.glsl
@@ -198,5 +198,5 @@ void main()
 	
 	frag_data[0] = vec4(color.rgb, color.a); // diffuse
 	frag_data[1] = vec4(0);		// speccolor, spec
-	frag_data[2] = vec4(encode_normal(screenspacewavef.xyz*0.5+0.5), 0.05, 0);// normalxy, 0, 0
+	frag_data[2] = vec4(encode_normal(screenspacewavef.xyz), 0.05, 0);// normalxy, 0, 0
 }
