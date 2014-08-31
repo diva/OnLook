8e45003c27738fe0eef6a228f5d9a9bd132ad256
Shyotl <Shyotl@gmail.com>
2014-08-31 04:56:55 -0500

    Packaging bugs. I'm drowning in them. (use subprocess.Popen instead of os.popen)

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -360,7 +360,7 @@ class WindowsManifest(ViewerManifest):
                     out_path = installed_dir
                     result += 'SetOutPath ' + out_path + '\n'
             if install:
-                result += 'File ' + pkg_file + '\n'
+                result += 'File "' + pkg_file + '"\n'
             else:
                 result += 'Delete ' + wpath(os.path.join('$INSTDIR', rel_file)) + '\n'
 
@@ -453,14 +453,14 @@ class WindowsManifest(ViewerManifest):
         try:
             import _winreg as reg
             NSIS_path = reg.QueryValue(reg.HKEY_LOCAL_MACHINE, r"SOFTWARE\NSIS\Unicode") + '\\makensis.exe'
-            self.run_command('"' + proper_windows_path(NSIS_path) + '" "' + self.dst_path_of(tempfile) + '"')
+            self.run_command([proper_windows_path(NSIS_path), self.dst_path_of(tempfile)])
         except:
             try:
                 NSIS_path = os.environ['ProgramFiles'] + '\\NSIS\\Unicode\\makensis.exe'
-                self.run_command('"' + proper_windows_path(NSIS_path) + '" "' + self.dst_path_of(tempfile) + '"')
+                self.run_command([proper_windows_path(NSIS_path), self.dst_path_of(tempfile)])
             except:
                 NSIS_path = os.environ['ProgramFiles(X86)'] + '\\NSIS\\Unicode\\makensis.exe'
-                self.run_command('"' + proper_windows_path(NSIS_path) + '" "' + self.dst_path_of(tempfile) + '"')
+                self.run_command([proper_windows_path(NSIS_path),self.dst_path_of(tempfile)])
         # self.remove(self.dst_path_of(tempfile))
         # If we're on a build machine, sign the code using our Authenticode certificate. JC
         sign_py = os.path.expandvars("{SIGN_PY}")
